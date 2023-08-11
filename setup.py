3bc970f633b04a7949abe46585223cd7f53a0112
Killian Sheriff <ksheriff@mit.edu>
2023-08-11 13:33:47 -0700

    long descp

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import os
+
 from setuptools import find_packages, setup
+
+# Get description from README
+root = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(root, "README.md"), "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="simplex",
@@ -7,7 +14,7 @@ setup(
     author="Killian Sheriff",
     author_email="ksheriff@mit.edu",
     description="A python package that implement the geometric object of a simplex.",
-    long_description="",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
     keywords=[],
